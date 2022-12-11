7a3af6eed3ab77c78c1f26ab6c13667cde6db6b7
Swastymaharani <swastymaharani09@gmail.com>
2022-12-11 21:04:12 +0800

    tes halo

--- resources/views/crud.blade.php
+++ resources/views/crud.blade.php
@@ -14,58 +14,8 @@
 @endif
 
 <div class="nk-fmg-body-head d-none d-lg-flex">
-    <div class="nk-fmg-search">
-        <h4 class="card-title text-primary"><i class='{{$icon}}' data-toggle='tooltip' data-placement='bottom' title='Data {{$subtitle}}'></i>  {{strtoupper("Data ".$subtitle)}}</h4>
-    </div>
-    <div class="nk-fmg-actions">
-        <div class="btn-group">
-            <a href="{{ route('crud.create') }}" class="btn btn-sm btn-primary" onclick="buttondisable(this)"><em class="icon fas fa-plus"></em> <span>Add Data</span></a>
-        </div>
-    </div>
+    halo
 </div>
-<div class="row gy-3 d-none" id="loaderspin">
-    <div class="col-md-12">
-        <div class="col-md-12" align="center">
-            &nbsp;
-        </div>
-        <div class="d-flex align-items-center">
-          <div class="col-md-12" align="center">
-            <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
-          </div>
-        </div>
-        <div class="col-md-12" align="center">
-            <strong>Loading...</strong>
-        </div>
-    </div>
-</div>
-<div class="card d-none" id="filterrow">
-    <div class="card-body" style="background:#f7f9fd">
-        <div class="row gy-3" >
-            
-        </div>
-    </div>
-</div>
-
-<!-- <div class="nk-fmg-body-content"> -->
-    <div class="nk-fmg-quick-list nk-block">
-        <div class="card">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table id="{{$table_id}}" class="small-table table " style="width:100%">
-                        <thead style="color:#526484; font-size:11px;" class="thead-light">
-                            <th width="1%">No.</th>
-                            <th width="10%">Nim</th>
-                            <th width="10%">Nama</th>
-                            <th width="10%">Alamat</th>
-                            <th width="10%">Aksi</th>
-                        </thead>
-                        <tbody></tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-<!-- </div> -->
 
 @endsection
 @push('script')
