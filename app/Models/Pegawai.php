c7965017c1a5fe9e948703e808ab2f6c1a59bb8f
putrisandya <putrisandya08@gmail.com>
2022-12-30 13:22:43 +0800

    revisi riwayat diklat

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -9,11 +9,13 @@ class Pegawai extends Model
 {
     use HasFactory;
     protected $table='m_pegawai';
+    protected $primarykey='id';
     protected $fillable=['kode', 'no_induk', 'absen_id', 'kode_bpjs','nama', 'nama_tercetak', 'gelar_depan', 'gelar_belakang', 
     'status_pegawai_id','jenis_profesi_id', 'spesialisasi_id','sub_spesialisasi_id','qualifikasi_id',
     'pendidikan_terakhir_id', 'jabatan_fungsional_terakhir','jabatan_struktural_id','unit_id',
     'subunit_id','tempat_lahir','tanggal_lahir','jeniskelamin_id','agama_id','bahasa_aktif_id','alamat','dusun','desa_id',
     'kecamatan_id','kabupaten_id','provinsi_id','kodepos','nik','npwp','file_photo','file_ktp','file_kk','file_npwp','status_nikah_id','status_daftar_id'];
+
 
     public function agama(){
         return $this->belongsTo(Agama::class);
@@ -85,5 +87,9 @@ class Pegawai extends Model
 
     public function unit(){
         return $this->belongsTo(Unit::class, 'unit_id');
+    }
+
+    public function riwayatDiklat(){
+        return $this->hasMany(RiwayatDiklat::class, 'id', 'id');
     }
 }
